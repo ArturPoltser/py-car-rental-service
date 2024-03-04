5404bee894b11c13ead62e8603600eea4ef473a2
Artur Poltser <polcerartur@gmail.com>
2024-03-04 18:28:28 +0200

    add models with validated fields to app rentals

--- rentals/models.py
+++ rentals/models.py
@@ -1,3 +1,83 @@
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Insurance(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    year = models.IntegerField(
+        validators=[
+            MinValueValidator(
+                limit_value=2005,
+                message="Year should be from 2005 onwards."
+            ),
+            MaxValueValidator(
+                limit_value=2023,
+                message="Year should not exceed 2023."
+            )
+        ]
+    )
+    horsepower = models.IntegerField(
+        validators=[
+            MinValueValidator(
+                limit_value=100,
+                message="We can't rent car with less than 100 horsepowers."
+            ),
+            MaxValueValidator(
+                limit_value=800,
+                message="We can't rent car with more than 800 horsepowers."
+            )
+        ]
+    )
+    fuel_consumption = models.DecimalField(max_digits=2, decimal_places=1)
+    daily_cost = models.IntegerField(
+        validators=[
+            MinValueValidator(
+                limit_value=100,
+                message="We can't rent car with daily price less than 100$."
+            ),
+            MaxValueValidator(
+                limit_value=2000,
+                message="We can't rent car with daily price more than 2000$."
+            )
+        ]
+    )
+    available_to_rent = models.BooleanField(default=True)
+    insurance = models.ForeignKey(Insurance, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.model
+
+
+class Rental(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    renter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def clean(self):
+        if self.end_date < self.start_date:
+            raise ValidationError("End date must not be earlier than the start date.")
+
+        total_period = (self.end_date - self.start_date).days
+        if total_period > 30:
+            raise ValidationError("The total rental period must not exceed 30 days.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Rental #{self.pk} - {self.car} by {self.renter}"
