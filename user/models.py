5ed94c3748bd84034bc6f6c9cb5b48a90bb05202
Artur Poltser <polcerartur@gmail.com>
2024-03-04 18:29:31 +0200

    add renter model with validated fields to app user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Renter(AbstractUser):
+    max_daily_budget = models.IntegerField(
+        validators=[
+            MinValueValidator(
+                limit_value=100,
+                message="We don't have such car for this budget. Sorry."
+            )
+        ]
+    )
+    driver_license = models.CharField(
+        max_length=255,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r"^[A-Z]{2}\d{5}$",
+                message="Driver license should consist only of 7 characters: "
+                        "first 2 characters are uppercase letters and "
+                        "last 5 characters are digits"
+            )
+        ]
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
