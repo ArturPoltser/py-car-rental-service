91c09b6c9cba59983ea31cc407f4533edfab3bc5
Artur Poltser <polcerartur@gmail.com>
2024-03-05 11:57:57 +0200

    add index url + index view

--- rentals/views.py
+++ rentals/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+from rentals.models import Car, Insurance
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_renters = get_user_model().objects.count()
+    num_cars = Car.objects.count()
+    num_insurances = Insurance.objects.count()
+
+    context = {
+        "num_renters": num_renters,
+        "num_cars": num_cars,
+        "num_insurances": num_insurances,
+    }
+
+    return render(request, "rentals/index.html", context=context)
