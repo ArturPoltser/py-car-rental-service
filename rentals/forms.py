9c31474041b364d49706039520268a087485f2b6
Artur Poltser <polcerartur@gmail.com>
2024-03-06 11:47:33 +0200

    create form for Rental model + rewrite CB to FB for rental model views

---
+++ rentals/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from rentals.models import Rental, Car
+
+
+class RentalForm(forms.ModelForm):
+    class Meta:
+        model = Rental
+        fields = ("start_date", "end_date")
+
+        widgets = {
+            "start_date": forms.DateInput(attrs={"type": "date"}),
+            "end_date": forms.DateInput(attrs={"type": "date"}),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get("start_date")
+        end_date = cleaned_data.get("end_date")
+
+        overlapping_rentals = Rental.objects.filter(
+            car=cleaned_data.get("car"),
+            start_date__lt=end_date,
+            end_date__gt=start_date
+        )
+
+        if overlapping_rentals.exists():
+            occupied_dates = overlapping_rentals.values_list(
+                "start_date",
+                "end_date"
+            )
+            occupied_dates_list = [
+                f"from {start_date} to {end_date}"
+                for start_date, end_date in occupied_dates
+            ]
+            raise forms.ValidationError(
+                "The selected dates are already occupied. "
+                f"Occupied dates: {', '.join(occupied_dates_list)}."
+            )
