f42cddab7ef74b276f3dfc9cf00c0c283b672899
Artur Poltser <polcerartur@gmail.com>
2024-03-06 14:26:19 +0200

    add Create/Update forms for user model + upgrade views

--- user/views.py
+++ user/views.py
@@ -3,6 +3,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views import generic
 
+from rentals.models import Rental
+from user.forms import (
+    RenterCreateForm,
+    RenterNameAndLicenseUpdateForm,
+)
+
 
 class RenterListView(LoginRequiredMixin, generic.ListView):
     model = get_user_model()
@@ -12,19 +18,26 @@ class RenterListView(LoginRequiredMixin, generic.ListView):
 class RenterDetailView(LoginRequiredMixin, generic.DetailView):
     model = get_user_model()
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["rental_history"] = Rental.objects.filter(renter=self.object)
+        return context
+
 
 class RenterCreateView(LoginRequiredMixin, generic.CreateView):
     model = get_user_model()
-    fields = "__all__"
-    success_url = reverse_lazy("")
+    form_class = RenterCreateForm
+    success_url = reverse_lazy("user:renter-list")
 
 
-class RenterUpdateView(LoginRequiredMixin, generic.CreateView):
+class RenterUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = get_user_model()
-    fields = "__all__"
-    success_url = reverse_lazy("")
+    form_class = RenterNameAndLicenseUpdateForm
+
+    def get_success_url(self):
+        return reverse_lazy("user:renter-detail", kwargs={"pk": self.object.pk})
 
 
 class RenterDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = get_user_model()
-    success_url = reverse_lazy("")
+    success_url = reverse_lazy("user:renter-list")
